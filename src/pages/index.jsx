adc4171fc72538bd6623f78b720098c3c4582838
Jheimys Bassani <jheimysbb@gmail.com>
2024-04-14 11:05:34 +0100

    solving bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Porta from "@/components/Porta";
-<<<<<<< HEAD
 import PortaModel from "../model/porta"
 import { useState } from "react";
 
@@ -10,14 +9,6 @@ export default function Home() {
   return (
     <div style={{display: "flex"}}>
       <Porta porta={p1} />
-=======
-
-
-export default function Home() {
-  return (
-    <div>
-      <Porta />
->>>>>>> 1cdd1421345e6ddab64d6debf45a376909bafb97
     </div>
   );
 }
